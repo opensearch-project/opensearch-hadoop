354b044acd1dabd01de400e25c4abdded14991bd
Harsha Vamsi Kalluri <harshavamsi096@gmail.com>
2023-05-16 18:12:01 -0700

    Feature/snapshotpublish (#224)
    * Disabled publish to generate POMs
    
    Signed-off-by: Harsha Vamsi Kalluri <harshavamsi096@gmail.com>
    
    * Disabled publish to generate POMs
    
    Signed-off-by: Harsha Vamsi Kalluri <harshavamsi096@gmail.com>
    
    * Fix dir structure
    
    Signed-off-by: Harsha Vamsi Kalluri <harshavamsi096@gmail.com>
    
    * Fix dir structure
    
    Signed-off-by: Harsha Vamsi Kalluri <harshavamsi096@gmail.com>
    
    * Attempt clean first
    
    Signed-off-by: Harsha Vamsi Kalluri <harshavamsi096@gmail.com>
    
    ---------
    
    Signed-off-by: Harsha Vamsi Kalluri <harshavamsi096@gmail.com>

--- buildSrc/src/main/groovy/org/opensearch/hadoop/gradle/BuildPlugin.groovy
+++ buildSrc/src/main/groovy/org/opensearch/hadoop/gradle/BuildPlugin.groovy
@@ -595,7 +595,7 @@ class BuildPlugin implements Plugin<Project>  {
         // If we are working with a project that has a scala variant (see below), we need to modify the pom's
         // artifact id which the publish task does not like (it fails validation when run).
         project.getTasks().withType(PublishToMavenRepository) { PublishToMavenRepository m ->
-            m.enabled = false
+            m.enabled = true
         }
 
         // Configure Scala Variants if present

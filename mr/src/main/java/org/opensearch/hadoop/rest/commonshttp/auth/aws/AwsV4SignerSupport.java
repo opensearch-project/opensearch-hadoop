2399e16a35154a2967d312f0fbcb5879015d9169
Thomas Farr <tsfarr@amazon.com>
2024-04-15 16:49:54 +1200

    Make SigV4 signing more thorough using validated signatures.
    Signature and example body used here:
    - https://github.com/opensearch-project/opensearch-rs/blob/main/opensearch/tests/aws_auth.rs#L43
    - https://github.com/opensearch-project/opensearch-net/blob/main/tests/Tests.Auth.AwsSigV4/AwsSigV4HttpConnectionTests.cs#L31
    
    Signed-off-by: Thomas Farr <tsfarr@amazon.com>

--- mr/src/main/java/org/opensearch/hadoop/rest/commonshttp/auth/aws/AwsV4SignerSupport.java
+++ mr/src/main/java/org/opensearch/hadoop/rest/commonshttp/auth/aws/AwsV4SignerSupport.java
@@ -10,60 +10,32 @@
 
 package org.opensearch.hadoop.rest.commonshttp.auth.aws;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
+import java.util.Date;
 import org.opensearch.hadoop.cfg.Settings;
 import org.opensearch.hadoop.rest.Request;
-import org.opensearch.hadoop.thirdparty.google.common.base.Optional;
+import org.opensearch.hadoop.thirdparty.apache.commons.httpclient.Header;
 import org.opensearch.hadoop.thirdparty.google.common.base.Splitter;
 import org.opensearch.hadoop.thirdparty.google.common.base.Strings;
-import org.opensearch.hadoop.thirdparty.google.common.base.Supplier;
-import org.opensearch.hadoop.thirdparty.google.common.collect.ImmutableListMultimap;
-import org.opensearch.hadoop.thirdparty.google.common.collect.Multimap;
-import org.opensearch.hadoop.thirdparty.google.common.collect.ImmutableMap;
-import org.opensearch.hadoop.thirdparty.google.common.collect.ImmutableList;
-import org.opensearch.hadoop.thirdparty.google.common.base.Joiner;
 import org.opensearch.hadoop.thirdparty.apache.commons.httpclient.HttpMethod;
 
 import com.amazonaws.DefaultRequest;
 import com.amazonaws.auth.AWS4Signer;
 import com.amazonaws.auth.AWSCredentials;
-import com.amazonaws.auth.AWSCredentialsProvider;
-import com.amazonaws.auth.AWSCredentialsProviderChain;
-import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
 import com.amazonaws.http.HttpMethodName;
-import com.amazonaws.regions.Regions;
-import com.amazonaws.util.SdkHttpUtils;
 import java.io.UnsupportedEncodingException;
 import java.net.URISyntaxException;
 import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.TreeMap;
-import org.opensearch.hadoop.util.ByteSequence;
 import java.util.List;
 import java.util.Map;
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.net.URLEncoder;
 
 public class AwsV4SignerSupport {
-
-    private static Log log = LogFactory.getLog(AwsV4SignerSupport.class);
-
     private final Settings settings;
     private final String httpInfo;
     private final AWSCredentials credentials;
-
-    private static final Joiner AMPERSAND_JOINER = Joiner.on('&');
-
-    public AwsV4SignerSupport(Settings settings, String httpInfo) {
-        this(settings, httpInfo, null);
-    }
+    private Date overriddenSigningDate;
 
     public AwsV4SignerSupport(Settings settings, String httpInfo, AWSCredentials credentials) {
         this.settings = settings;
@@ -71,15 +43,12 @@ public class AwsV4SignerSupport {
         this.credentials = credentials;
     }
 
-    private String queryParamsString(Multimap<String, String> queryParams) {
-        final ImmutableList.Builder<String> result = ImmutableList.builder();
-        for (Map.Entry<String, Collection<String>> param : new TreeMap<>(queryParams.asMap()).entrySet()) {
-            for (String value : param.getValue()) {
-                result.add(SdkHttpUtils.urlEncode(param.getKey(), false) + '=' + SdkHttpUtils.urlEncode(value, false));
-            }
-        }
-
-        return AMPERSAND_JOINER.join(result.build());
+    /**
+     * Overrides the signing date for the request for testing purposes.
+     * @param signingDate The fixed signing date to use.
+     */
+    void overrideSigningDate(Date signingDate) {
+        this.overriddenSigningDate = signingDate;
     }
 
     public void sign(Request request, HttpMethod http)
@@ -91,11 +60,12 @@ public class AwsV4SignerSupport {
         aws4Signer.setRegionName(awsRegion);
         aws4Signer.setServiceName(awsServiceName);
 
+        if (overriddenSigningDate != null) {
+            aws4Signer.setOverrideDate(overriddenSigningDate);
+        }
+
         DefaultRequest<Void> req = new DefaultRequest<>(awsServiceName);
         req.setHttpMethod(HttpMethodName.valueOf(request.method().name()));
-
-        StringBuilder url = new StringBuilder();
-        url.append(httpInfo);
 
         String path = request.path().toString();
         req.setResourcePath(path);
@@ -113,9 +83,9 @@ public class AwsV4SignerSupport {
                 if (index > 0) {
                     final String key = pair.substring(0, index);
                     final String value = pair.substring(index + 1);
-                    queryParams.put(key, Arrays.asList(value));
+                    queryParams.put(key, Collections.singletonList(value));
                 } else {
-                    queryParams.put(pair, Arrays.asList(""));
+                    queryParams.put(pair, Collections.singletonList(""));
                 }
             }
         }
@@ -123,13 +93,19 @@ public class AwsV4SignerSupport {
         req.setParameters(queryParams);
 
         try {
-            req.setEndpoint(new java.net.URI(url.toString()));
+            req.setEndpoint(new java.net.URI(httpInfo));
         } catch (URISyntaxException e) {
-            throw new IllegalArgumentException("Invalid request URI: " + request.uri().toString());
+            throw new IllegalArgumentException("Invalid request URI: " + httpInfo);
         }
 
         if (request.body() != null) {
             req.setContent(request.body().toInputStream());
+        }
+
+        for (Header header : http.getRequestHeaders()) {
+            if (header.getName().equalsIgnoreCase("user-agent")) continue;
+
+            req.addHeader(header.getName(), header.getValue());
         }
 
         req.addHeader("x-amz-content-sha256", "required");
